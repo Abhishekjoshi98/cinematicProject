b64804059662fd7601ca2656e0e6969e5fac39b6
Abhishekjoshi98 <98544171+Abhishekjoshi98@users.noreply.github.com>
2023-01-12 20:35:50 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = _ => {
   return (
     <Box pb={4}>
       <Typography variant='h6' align='center'>
-        <a href="https://github.com/aayusharyan/cinematic" target="_blank" rel="noreferrer">
+        <a href="https://github.com/Abhishekjoshi98/cinematicProject/tree/main/src" target="_blank" rel="noreferrer">
           Github Source Code
         </a>
       </Typography>
